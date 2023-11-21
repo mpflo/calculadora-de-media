3015713b36bd991b5e027ac963350b72ef7aed4b
mpflo <qipeot@gmail.com>
2023-11-21 10:30:07 -0300

    mudei pasta de nivel-2 para nivel-2-2

--- nivel-2-2/calculadora-media.js
+++ nivel-2-2/calculadora-media.js
@@ -1,11 +1,18 @@
 const input = require('readline-sync')
 
 let qntdAlunosAvaliados = Number(input.question('Quantos alunes serao avaliades? '))
+let qtdNotas = Number(input.question('Quantas notas vao ser calculadas? '))
 
+let alunos = []
+
+// coletando dados
 for (let numAluno = 1; numAluno <= qntdAlunosAvaliados; numAluno++) {
     let nome = input.question(`Qual e o nome da aluna ${numAluno} avaliada? `)
-    let qtdNotas = Number(input.question('Quantas notas vao ser calculadas? '))
-
+    const aluno = {
+        nome: nome,
+        media: 0
+    }
+    
     // Criando uma variável neutra para a soma das notas
     let somatorio = 0
 
@@ -15,17 +22,25 @@ for (let numAluno = 1; numAluno <= qntdAlunosAvaliados; numAluno++) {
     }
 
     // Dividindo essa nota 
-    let media = (somatorio / qtdNotas).toFixed(2) // deixa com duas casas decimais
-    console.log(`${nome} tirou ${media} nesse semestre.`)
-
-    if (media >= 7) {
-        console.log(`${nome} foi aprovade.`)
-    } else if (media >= 5) {
-        console.log(`${nome} está de recuperação.`)
-    } else {
-        console.log(`${nome} foi reprovade.`)
-    }
+    aluno.media = (somatorio / qtdNotas).toFixed(2) // deixa com duas casas decimais
     
+    alunos.push(aluno) // empurrando para o final da lista
+}
+
+
+// exibindo resultados | dica: poderia ser mais legível com for of
+for (let indexAluno = 0; indexAluno < qntdAlunosAvaliados; indexAluno++) {
+    const aluno = alunos[indexAluno];
+
+    console.log(`${aluno.nome} tirou ${aluno.media} nesse semestre.`)
+
+    if (aluno.media >= 7) {
+        console.log(`${aluno.nome} foi aprovade.`)
+    } else if (aluno.media >= 5) {
+        console.log(`${aluno.nome} está de recuperação.`)
+    } else {
+        console.log(`${aluno.nome} foi reprovade.`)
+    }
 }
 
 
