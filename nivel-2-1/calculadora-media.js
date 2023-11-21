edcb6ab4f61d91c2995fd49d27baa4347f13fec5
mpflo <qipeot@gmail.com>
2023-11-21 10:29:11 -0300

    mudei pasta de nivel-2-2 p/ nivel-2-1

---
+++ nivel-2-1/calculadora-media.js
@@ -0,0 +1,32 @@
+const input = require('readline-sync')
+
+let qntdAlunosAvaliados = Number(input.question('Quantos alunes serao avaliades? '))
+
+for (let numAluno = 1; numAluno <= qntdAlunosAvaliados; numAluno++) {
+    let nome = input.question(`Qual e o nome da aluna ${numAluno} avaliada? `)
+    let qtdNotas = Number(input.question('Quantas notas vao ser calculadas? '))
+
+    // Criando uma variável neutra para a soma das notas
+    let somatorio = 0
+
+    for (let numProva = 1; numProva <= qtdNotas; numProva++) {
+        let nota = Number(input.question(`Digite a nota ${numProva}: `))
+        somatorio = somatorio + nota
+    }
+
+    // Dividindo essa nota 
+    let media = (somatorio / qtdNotas).toFixed(2) // deixa com duas casas decimais
+    console.log(`${nome} tirou ${media} nesse semestre.`)
+
+    if (media >= 7) {
+        console.log(`${nome} foi aprovade.`)
+    } else if (media >= 5) {
+        console.log(`${nome} está de recuperação.`)
+    } else {
+        console.log(`${nome} foi reprovade.`)
+    }
+    
+}
+
+
+
